0883b8b0d66442b1690db436fa035adfcd6db56a
wozbott <awizardnamedwoz@gmail.com>
2019-04-27 13:27:38 -0500

    Additional movement and add wasd control.

--- scenes/entities/player/Player.gd
+++ scenes/entities/player/Player.gd
@@ -19,6 +19,10 @@ func _process(delta):
 		velocity.x += 1
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -33,7 +37,6 @@ func _process(delta):
 		$AnimatedSprite.flip_v = false
 		$AnimatedSprite.flip_h = velocity.x < 0
 	elif velocity.y != 0:
-		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 		
 func start(pos):
